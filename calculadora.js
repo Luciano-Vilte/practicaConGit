c2b098320a8dc20a5856e85137fd231f084f86fd
Luciano Vilte <lucianovilte111@gmail.com>
2023-11-15 21:17:08 -0300

    hice el array con las funciones

--- calculadora.js
+++ calculadora.js
@@ -3,3 +3,9 @@ const rest = require("./restar")
 const mult = require("./multiplicar")
 const div = require("./dividir")
 
+const sumar = sum();
+const restar = rest();
+const multiplicar = mult();
+const dividir = div();
+
+const operaciones = [sumar, restar, multiplicar, dividir];
