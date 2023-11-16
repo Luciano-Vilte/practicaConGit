9e9fdd7d66cc3f947a519bedc56c80d2dd63766c
Crasthiff <fotin9@gmail.com>
2023-11-15 21:32:31 -0300

    arreglé el archivo calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -3,10 +3,14 @@ const rest = require("./restar")
 const mult = require("./multiplicar")
 const div = require("./dividir")
 
-const sumar = sum();
-const restar = rest();
-const multiplicar = mult();
-const dividir = div();
+const sumar = sum.suma(5,10);
+const restar = rest.restar(10,5);
+const multiplicar = mult.multiplicar(10,2);
+const dividir = div.dividir(10,5);
 
-const operaciones = [sumar, restar, multiplicar, dividir];
+// const operaciones = [sumar, restar, multiplicar, dividir];
  
+console.log(sumar)
+console.log(restar)
+console.log(multiplicar)
+console.log(dividir)
